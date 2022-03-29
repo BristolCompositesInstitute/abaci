0b20d24236ea0d9d26b23dbe97e0060be4d6f5f6
Laurence Kedward <laurence.kedward@bristol.ac.uk>
2022-03-29 11:22:36 +0100

    Add: git_utils module for interfacing with git

---
+++ src/abaci/git_utils.py
@@ -0,0 +1,68 @@
+import os
+import tempfile
+from abaci.utils import cwd, system_cmd, system_cmd_wait
+
+def clone(git_path,working_dir,target_dir,verbosity):
+    """Clone a git repository"""
+    fh,git_log_file = tempfile.mkstemp()
+    os.close(fh)
+
+    with cwd(working_dir):
+
+        git_cmd = ['git', 'clone', git_path, target_dir]
+
+        p, ofile, efile = system_cmd(git_cmd,output=git_log_file)
+        
+        stat = system_cmd_wait(p, verbosity, ofile, efile)
+
+        if stat:
+
+            raise Exception('Error while cloning repository {repo}'.format(repo=dep_git))
+
+
+def checkout(git_path,git_ref,verbosity):
+    """Checkout a branch/tag/commit in an existing local repository"""
+    fh,git_log_file = tempfile.mkstemp()
+    os.close(fh)
+
+    with cwd(git_path):
+
+        git_cmd = ['git', 'checkout', git_ref]
+
+        p, ofile, efile = system_cmd(git_cmd,output=git_log_file)
+        
+        stat = system_cmd_wait(p, verbosity, ofile, efile)
+
+        if stat:
+
+            raise Exception('Error while checking out reference {ref} in {repo}'.format(
+                                ref=git_ref,repo=git_path))
+
+    return stat
+
+
+def is_head_detached(git_path):
+    """Check if HEAD is detached in a local repository"""
+    with cwd(git_path):
+
+        git_cmd = ['git', 'symbolic-ref', '-q', 'HEAD']
+        
+        p, ofile, efile = system_cmd(git_cmd)
+                
+        stat = system_cmd_wait(p, verbosity=-1)
+
+    return stat != 0
+
+
+def is_dirty(git_path):
+    """Check if git repository has unstaged changes"""
+
+    with cwd(git_path):
+
+        git_cmd = ['git', 'diff', '--quiet']
+        
+        p, ofile, efile = system_cmd(git_cmd)
+                
+        stat = system_cmd_wait(p, verbosity=-1)
+
+    return stat != 0
