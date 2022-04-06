5146c8c020d7d76a88204abd3901ff65902eee4b
Laurence Kedward <laurence.kedward@bristol.ac.uk>
2022-04-06 11:42:21 +0100

    Fix: issues with CTRL+C interrupt on windows.

--- scripts/abaci.cmd
+++ scripts/abaci.cmd
@@ -1,16 +1,22 @@
 @echo off
 
+setlocal
+
+REM Prevent Intel run-time library from establishing signal handlers
+REM  to allow handling CTRL+C SIGINT in abaci
+set FOR_DISABLE_CONSOLE_CTRL_HANDLER=1
+
 if exist %~dp0..\src\abaci_main.py (
 
     REM Relative execution path
-    abaqus python %~dp0..\src\abaci_main.py %*
+    abaqus python %~dp0..\src\abaci_main.py %* < nul
     
 ) else (
 
     if exist %LOCALAPPDATA%\BCI\abaci\src\abaci_main.py (
 
         REM Local installation path
-        abaqus python %LOCALAPPDATA%\BCI\abaci\src\abaci_main.py %*
+        abaqus python %LOCALAPPDATA%\BCI\abaci\src\abaci_main.py %* < nul
 
     ) else (
 
@@ -19,3 +25,5 @@ if exist %~dp0..\src\abaci_main.py (
     )
 
 )
+
+endlocal
