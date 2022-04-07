3c20fa1d77be3691e2656e1c8ce1fa21ecbfe01c
LKedward <laurence.kedward@bristol.ac.uk>
2022-04-07 15:18:44 +0100

    Update: ssh_utils to support Git for Windows setup Looks for ssh utilities in the default install location for Git for Windows.

--- src/abaci/ssh_utils.py
+++ src/abaci/ssh_utils.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import cPickle as pkl
 from tempfile import gettempdir
+import logging
 from redist import parse_agent_variables
 
 ssh_agent_cache_file = os.path.join(gettempdir(),'abaci-ssh-agent-cache')
@@ -13,24 +14,31 @@ def setup_ssh_agent():
         per session
 
     """
+    
+    log = logging.getLogger('abaci')
+    
     agent_data = get_existing_agent()
 
     if not agent_data:
 
         agent_data = start_ssh_agent()
-
+    
+    else:
+        
+        log.debug('Found existing ssh agent (pid=%s)',agent_data['SSH_AGENT_PID'])
+        
     set_envvars(agent_data)
 
     if not is_identity_added():
-        subprocess.check_call(['ssh-add'])
+        subprocess.check_call([get_ssh_cmd('ssh-add')])
 
 
 def is_identity_added():
     """Check if the default ssh identity has been added to the ssh-agent"""
 
     devnull = open(os.devnull,'w')
-
-    cmd = ['ssh-add', '-l']
+    
+    cmd = [get_ssh_cmd('ssh-add'), '-l']
                 
     stat =  subprocess.call(cmd,stdout=devnull,stderr=devnull)
     
@@ -41,10 +49,14 @@ def start_ssh_agent():
     """Start a new ssh-agent and return the agent_data struct"""
 
     global ssh_agent_cache_file
-
-    p = subprocess.Popen(['ssh-agent', '-s'], stdout=subprocess.PIPE)
-    stdout, stderr = p.communicate()
-
+    
+    log = logging.getLogger('abaci')
+    
+    log.info('Starting ssh-agent')
+    
+    cmd = [get_ssh_cmd('ssh-agent'), '-s']
+    stdout = subprocess.check_output(cmd).strip()
+    
     agent_data = parse_agent_variables.parse(stdout)
     
     with open(ssh_agent_cache_file,'w') as f:
@@ -52,6 +64,19 @@ def start_ssh_agent():
 
     return agent_data
 
+    
+def get_ssh_cmd(cmd):
+    
+    if os.name != 'nt':
+        return cmd
+    
+    search_locations = ['C:\\Program Files\\Git\\usr\\bin\\','']
+    
+    for loc in search_locations:
+        if os.path.exists(loc+cmd+'.exe'):
+            
+            return loc+cmd
+    
 
 def set_envvars(agent_data):
     """Setup environment variables from agent_data struct"""
@@ -91,13 +116,7 @@ def get_existing_agent():
 def check_agent(pid):
     """Check if PID corresponds to existing ssh-agent instance"""
     
-    if os.name == 'nt':
-
-        cmd = ['tasklist', '/fi', 'pid eq {pid}'.format(pid=pid)]
-
-    else:
-
-        cmd = ['ps', '-p', '{pid}'.format(pid=pid)]
+    cmd = [get_ssh_cmd('ps'), '-p', '{pid}'.format(pid=pid)]
 
     try:
 
@@ -106,7 +125,7 @@ def check_agent(pid):
     except:
 
         output = ''
-
+    
     return 'ssh-agent' in output
 
 
