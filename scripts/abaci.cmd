d937d5ea6893f5cd43bc726f2e0d07a6ff3714b4
Laurence Kedward <laurence.kedward@bristol.ac.uk>
2022-06-14 17:08:48 +0100

    Fix: for utf8 encoding on Windows Fixes crash due to config file encoding.

--- scripts/abaci.cmd
+++ scripts/abaci.cmd
@@ -6,6 +6,8 @@ REM Prevent Intel run-time library from establishing signal handlers
 REM  to allow handling CTRL+C SIGINT in abaci
 set FOR_DISABLE_CONSOLE_CTRL_HANDLER=1
 
+set PYTHONIOENCODING=utf8
+
 if exist %~dp0..\src\abaci_main.py (
 
     REM Relative execution path
