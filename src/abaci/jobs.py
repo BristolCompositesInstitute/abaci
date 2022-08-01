BristolCompositesInstitute/abaci

commit 672f48ee8bd67269ca86f76461d41e3bbfb59739

Add: --screen arg to echo Abaqus output while running

src/abaci/jobs.py
@@ -110,10 +110,10 @@ def run_jobs(args,compile_dir,jobs):
     stats = []
 
 
-    # Wait for all jobs to complete
+    # Poll jobs for output and to check for completion time
     while True:
 
-        n_running = sum([j.is_running() for j in launched])
+        n_running = sum([j.poll(args.screen_output) for j in launched])
 
         if n_running == 0:
             break

src/abaci/AbaqusJob.py
@@ -55,7 +55,8 @@ class AbaqusJob:
         self.start_time = None
         self.end_time = None
         self.job_script = None
-        
+        self.ofile_handle = None
+        self.efile_handle = None
 
     def get_new_job_dir(self,output_dir):
         """Find a new job directory to run job in"""
@@ -88,10 +89,11 @@ class AbaqusJob:
                                           nproc=nproc)
 
 
-    def is_running(self):
+    def poll(self, screen_output=None):
         """
-        Check if job is currently running and record end time if not
-        (The resolution of recorded end time is how frequently you poll this function)
+        Check if job is currently running, dump latest output if requested,
+        and record end time at completion.
+        (The resolution of recorded end time is how frequently you call this function)
         """
 
         log = logging.getLogger('abaci')
@@ -100,6 +102,32 @@ class AbaqusJob:
 
         has_started = self.start_time
 
+        if has_started and not self.ofile_handle:
+
+            self.ofile_handle = open(self.ofile,'r')
+
+        if has_started and not self.efile_handle:
+
+            self.efile_handle = open(self.efile,'r')
+
+        # Print latest output from stdout & stderr logs
+        if has_started and screen_output:
+
+            for handle in [self.ofile_handle, self.efile_handle]:
+
+                while True:
+
+                    line = handle.readline()
+
+                    if line:
+
+                        print line.strip()
+
+                    else:
+
+                        break
+        
+        # Record end time
         if has_started and (not is_running) and \
             (not self.end_time):
 
@@ -118,7 +146,7 @@ class AbaqusJob:
 
         log = logging.getLogger('abaci')
 
-        if self.is_running():
+        if self.poll():
 
             # Job not finished yet
             log.info('Waiting for job "%s" to complete...', self.name)
@@ -133,7 +161,7 @@ class AbaqusJob:
 
         log = logging.getLogger('abaci')
 
-        if not self.is_running():
+        if not self.poll():
             
             # Job already completed
             return

src/abaci/cli.py
@@ -85,6 +85,9 @@ def parse_cli():
     build_group.add_argument('-g','--gcc',help='use gnu compilers for auxillary source files',
                         dest='gcc',action='store_true')
 
+    build_group.add_argument('-s','--screen',help='echo Abaqus output to the screen while running',
+                        dest='screen_output',action='store_true')
+
     # SUBMIT subcommand
     submit_command = subparsers.add_parser('submit', parents=[common_group,build_group],
                                          help='Compile user subroutines and submit jobs to cluster (SLURM)',

src/abaci/compile.py
@@ -38,7 +38,7 @@ def compile_user_subroutine(args, output_dir, user_file, compile_conf, dep_list)
 
     log.info('Running abaqus make')
 
-    stat = abq.make(dir=compile_dir, lib_file=compile_file, verbosity=args.verbose)
+    stat = abq.make(dir=compile_dir, lib_file=compile_file, verbosity=(args.verbose + 2*args.screen_output))
     
     return stat, compile_dir
 
@@ -334,7 +334,7 @@ def spool_env_file(compile_dir,flags):
 
         f.write('compile_fortran.extend({flags})\n'.format(flags=flags.__str__()))
 
-        f.write('print(compile_fortran)\n')
+        f.write('print(" ".join(compile_fortran))\n')
 
 
 def collect_cov_report(config,compile_dir,verbosity):

commit f0d1b0943641b986a0b517e85b06fb03fdcff1d1

Fix: bug for concurrently running jobs. Bug introduced in 672f48ee8bd67269ca86f76461d41e3bbfb59739 when renaming AbaqusJob.is_running to AbaqusJob.poll().

src/abaci/jobs.py
@@ -94,7 +94,7 @@ def run_jobs(args,compile_dir,jobs):
 
         # Wait here if already running maximum number of concurrent jobs
         while True:
-            n_running = sum([j.is_running() for j in launched])
+            n_running = sum([j.poll() for j in launched])
 
             if n_running < args.njob:
                 break